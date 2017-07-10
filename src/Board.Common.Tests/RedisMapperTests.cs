9ca8d693d6b708582199a029ea2b4307cb8e1e52
Zoltán Tóth <zshun86@gmail.com>
2017-07-10 19:25:28 +0200

    RedisMapper support for string collections added. And tests too.

---
+++ src/Board.Common.Tests/RedisMapperTests.cs
@@ -0,0 +1,167 @@
+﻿using System;
+using System.Collections.Generic;
+using Board.Common.Utils;
+using StackExchange.Redis;
+using Xunit;
+
+namespace Board.Common.Tests
+{
+    public class TestType1
+    {
+        public int IntegerProperty { get; set; }
+        public long LongProperty { get; set; }
+        public float FloatProperty { get; set; }
+        public double DoubleProperty { get; set; }
+        public bool BoolProperty { get; set; }
+        public string StringProperty { get; set; }
+        public byte[] ByteArrayProperty { get; set; }
+        public DateTime DateTimeProperty { get; set; }
+        public Guid GuidProperty { get; set; }
+    }
+
+    public class UnsupportedPropertyTestType1
+    {
+        public int IntegerProperty { get; set; }
+        public object UnsupportedProperty { get; set; }
+    }
+
+    public class UnsupportedPropertyTestType2
+    {
+        public int IntegerProperty { get; set; }
+        public ICollection<int> UnsupportedProperty { get; set; }
+    }
+
+    public class CollectionOfStringsTestType1
+    {
+        public ICollection<string> StringCollection { get; set; }
+    }
+
+    public class CollectionOfStringsTestType2
+    {
+        public List<string> StringCollection { get; set; }
+    }
+
+    public class RedisMapperTests : IDisposable
+    {
+        public void Dispose()
+        {
+            RedisMapper.UnregisterAll();
+        }
+
+        [Fact]
+        public void RegisterType_should_register_public_get_set_properties()
+        {
+            RedisMapper.RegisterType<TestType1>();
+            Assert.Equal(9, RedisMapper.RegisteredTypes[typeof(TestType1)].Count);
+        }
+
+        [Fact]
+        public void RegisterType_should_register_ICollection_of_string_properties()
+        {
+            RedisMapper.RegisterType<CollectionOfStringsTestType1>();
+            Assert.Contains(RedisMapper.RegisteredTypes[typeof(CollectionOfStringsTestType1)], props => props.Key == "StringCollection" && props.Value == typeof(ICollection<string>));
+
+            RedisMapper.RegisterType<CollectionOfStringsTestType2>();
+            Assert.Contains(RedisMapper.RegisteredTypes[typeof(CollectionOfStringsTestType2)], props => props.Key == "StringCollection" && props.Value == typeof(List<string>));
+        }
+
+        [Fact]
+        public void RegisterType_should_throw_by_default_when_an_unsupported_type_is_detected()
+        {
+            Assert.Throws<ArgumentException>(() => RedisMapper.RegisterType<UnsupportedPropertyTestType1>());
+            Assert.Throws<ArgumentException>(() => RedisMapper.RegisterType<UnsupportedPropertyTestType2>());
+        }
+        
+        [Fact]
+        public void ToRedis_should_be_able_to_handle_an_empty_collection_of_strings()
+        {
+            RedisMapper.RegisterType<CollectionOfStringsTestType1>();
+            Assert.Equal(1, RedisMapper.RegisteredTypes[typeof(CollectionOfStringsTestType1)].Count);
+
+            RedisMapper.RegisterType<CollectionOfStringsTestType2>();
+            Assert.Equal(1, RedisMapper.RegisteredTypes[typeof(CollectionOfStringsTestType2)].Count);
+
+            var subject = new CollectionOfStringsTestType2
+            {
+                StringCollection = new List<string>()
+            };
+
+            var result = subject.ToRedis();
+            Assert.Equal($"{typeof(List<string>).AssemblyQualifiedName}", result[0].Value);
+        }
+
+        [Fact]
+        public void ToRedis_should_be_able_to_handle_collection_of_strings()
+        {
+            RedisMapper.RegisterType<CollectionOfStringsTestType1>();
+            Assert.Equal(1, RedisMapper.RegisteredTypes[typeof(CollectionOfStringsTestType1)].Count);
+
+            RedisMapper.RegisterType<CollectionOfStringsTestType2>();
+            Assert.Equal(1, RedisMapper.RegisteredTypes[typeof(CollectionOfStringsTestType2)].Count);
+
+            var subject = new CollectionOfStringsTestType2
+            {
+                StringCollection = new List<string>
+                {
+                    "item1",
+                    "item2",
+                    "item3",
+                    @"|item4"
+                }
+            };
+
+            var result = subject.ToRedis();
+            Assert.Equal($@"{typeof(List<string>).AssemblyQualifiedName}||item1||item2||item3||\|item4", result[0].Value);
+        }
+        
+        [Fact]
+        public void FromRedis_should_return_an_object_of_a_supported_type_without_a_collection()
+        {
+            RedisMapper.RegisterType<TestType1>();
+            var subject = new[]
+            {
+                new HashEntry("IntegerProperty", 1),
+                new HashEntry("FloatProperty", 2.0f),
+                new HashEntry("DoubleProperty", 3.0d),
+                new HashEntry("BoolProperty", true),
+                new HashEntry("StringProperty", "teszt"),
+                new HashEntry("ByteArrayProperty", new byte[]{0, 1, 2}),
+                new HashEntry("DateTimeProperty", new DateTime(2000, 1, 1, 12, 0, 0).ToString("O")),
+                new HashEntry("GuidProperty", Guid.Empty.ToString())
+            };
+
+            var result = Assert.IsType<TestType1>(subject.FromRedis<TestType1>());
+            Assert.Equal(1, result.IntegerProperty);
+            Assert.Equal(2.0f, result.FloatProperty);
+            Assert.Equal(3.0d, result.DoubleProperty);
+            Assert.Equal(true, result.BoolProperty);
+            Assert.Equal("teszt", result.StringProperty);
+            Assert.Equal(3, result.ByteArrayProperty.Length);
+            Assert.Equal(0, result.ByteArrayProperty[0]);
+            Assert.Equal(1, result.ByteArrayProperty[1]);
+            Assert.Equal(2, result.ByteArrayProperty[2]);
+            Assert.Equal(new DateTime(2000, 1, 1, 12, 0, 0), result.DateTimeProperty);
+            Assert.Equal(Guid.Empty, result.GuidProperty);
+        }
+
+        [Fact]
+        public void FromRedis_should_return_an_object_of_a_supported_type_with_an_ICollection_of_strings()
+        {
+            RedisMapper.RegisterType<CollectionOfStringsTestType1>();
+            var subject = new[]
+            {
+                new HashEntry(
+                    "StringCollection",
+                    $@"{typeof(List<string>).AssemblyQualifiedName}||item1||item2||item3||\|item4")
+            };
+
+            var result = Assert.IsType<CollectionOfStringsTestType1>(subject.FromRedis<CollectionOfStringsTestType1>());
+
+            Assert.Equal(4, result.StringCollection.Count);
+            Assert.Contains("item1", result.StringCollection);
+            Assert.Contains("item2", result.StringCollection);
+            Assert.Contains("item3", result.StringCollection);
+            Assert.Contains("|item4", result.StringCollection);
+        }
+    }
+}
--- src/Board.Common/Utils/RedisMapper.cs
+++ src/Board.Common/Utils/RedisMapper.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
+using System.Text;
 using FastMember;
 using StackExchange.Redis;
 
@@ -9,7 +10,7 @@ namespace Board.Common.Utils
 {
     public static class RedisMapper
     {
-        public static Dictionary<Type, Dictionary<string, Type>> TypeProperties = new Dictionary<Type, Dictionary<string, Type>>();
+        public static Dictionary<Type, Dictionary<string, Type>> RegisteredTypes = new Dictionary<Type, Dictionary<string, Type>>();
         private static readonly Dictionary<Type, Func<object, RedisValue>> ToRedisValueConverters = new Dictionary<Type, Func<object, RedisValue>>
         {
             { typeof(int), o => (int)o },
@@ -22,7 +23,7 @@ namespace Board.Common.Utils
             { typeof(DateTime), o => ((DateTime)o).ToString("O") },
             { typeof(Guid), o => o.ToString() }
         };
-        private static readonly Dictionary<Type, Func<RedisValue, object>> FromRedisValueConverters = new Dictionary<Type, Func<RedisValue, object>>
+        private static readonly Dictionary<Type, Func<RedisValue, object>> RedisConverters = new Dictionary<Type, Func<RedisValue, object>>
         {
             { typeof(int), o => (int)o },
             { typeof(float), o => (float)o },
@@ -35,39 +36,59 @@ namespace Board.Common.Utils
             { typeof(Guid), o => new Guid(o.ToString()) }
         };
 
-        public static void RegisterType<T>()
+        public static void UnregisterAll()
         {
-            var getSetProperties = typeof(T).GetProperties(BindingFlags.DeclaredOnly | 
-                                                     BindingFlags.Public |
-                                                     BindingFlags.Instance |
-                                                     BindingFlags.GetProperty | 
-                                                     BindingFlags.SetProperty)
-                                            .Where(p => p.GetSetMethod() != null);
-            RegisterType<T>(getSetProperties.Select(p => p.Name).ToArray());
+            RegisteredTypes.Clear();
         }
 
-        public static void RegisterType<T>(string[] properties)
+        public static void RegisterType<T>(bool throwOnUnsupportedProperty = true)
+        {
+            var getSetProperties = typeof(T).GetProperties(BindingFlags.DeclaredOnly |
+                                                     BindingFlags.Public |
+                                                     BindingFlags.Instance |
+                                                     BindingFlags.GetProperty |
+                                                     BindingFlags.SetProperty)
+                                            .Where(p => p.GetSetMethod() != null);
+            RegisterType<T>(getSetProperties.Select(p => p.Name).ToArray(), throwOnUnsupportedProperty);
+        }
+
+        public static void RegisterType<T>(string[] properties, bool throwOnUnsupportedProperty = true)
         {
             var typeProperties = new Dictionary<string, Type>();
             foreach (var property in properties)
             {
-                typeProperties.Add(property, typeof(T).GetProperty(property).PropertyType);
+                Type propertyType = typeof(T).GetProperty(property).PropertyType;
+                if (throwOnUnsupportedProperty &&
+                    !ImplementsICollectionOfStrings(propertyType) &&
+                    !RedisConverters.ContainsKey(propertyType))
+                {
+                    throw new ArgumentException("Unsupported property type detected on property: {0}", property);
+                }
+                typeProperties.Add(property, propertyType);
             }
 
-            TypeProperties.Add(typeof(T), typeProperties);
+            RegisteredTypes.Add(typeof(T), typeProperties);
         }
 
         public static HashEntry[] ToRedis<T>(this T @object)
         {
             var objectType = typeof(T);
-            if (TypeProperties.ContainsKey(objectType))
+            if (RegisteredTypes.ContainsKey(objectType))
             {
                 var objectAccessor = ObjectAccessor.Create(@object);
                 var hashEntries = new List<HashEntry>();
-                foreach (var properties in TypeProperties[objectType])
+                foreach (var properties in RegisteredTypes[objectType])
                 {
                     var hashKey = properties.Key;
-                    var hashValue = ToRedisValueConverters[properties.Value](objectAccessor[properties.Key]);
+                    RedisValue hashValue;
+                    if (ImplementsICollectionOfStrings(properties.Value))
+                    {
+                        hashValue = CollectionToRedisValue((ICollection<string>) objectAccessor[properties.Key]);
+                    }
+                    else
+                    {
+                        hashValue = ToRedisValueConverters[properties.Value](objectAccessor[properties.Key]);
+                    }
                     hashEntries.Add(new HashEntry(hashKey, hashValue));
                 }
                 return hashEntries.ToArray();
@@ -75,18 +96,65 @@ namespace Board.Common.Utils
             throw new ArgumentException("There is no mapping defined for this object. Use the RedisMapper.RegisterType() method to do so.");
         }
 
+        private static RedisValue CollectionToRedisValue(ICollection<string> stringCollection)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.Append(stringCollection.GetType().AssemblyQualifiedName);
+            sb.Append("||");
+
+            foreach (var str in stringCollection)
+            {
+                sb.Append(str.Replace("|", @"\|"));
+                sb.Append("||");
+            }
+            return sb.ToString(0, sb.Length - 2);
+        }
+
+        private static ICollection<string> RedisValueToCollection(string redisValue)
+        {
+            var splitString = redisValue.Split(new[] {"||"}, StringSplitOptions.None);
+
+            string typeName = splitString[0];
+
+            var collection = (ICollection<string>) Activator.CreateInstance(Type.GetType(typeName));
+
+            for (int i = 1; i < splitString.Length; i++)
+            {
+                collection.Add(splitString[i].Replace(@"\|", "|"));
+            }
+            return collection;
+        }
+
+        private static bool ImplementsICollectionOfStrings(Type t)
+        {
+            return t.IsConstructedGenericType && t.GetGenericTypeDefinition() == typeof(ICollection<>) && t.GenericTypeArguments[0] == typeof(string) ||
+                t.GetInterfaces().Any(x =>
+                x.IsConstructedGenericType &&
+                    x.GetGenericTypeDefinition() == typeof(ICollection<>) &&
+                    x.GenericTypeArguments[0] == typeof(string)
+            );
+        }
+
         public static T FromRedis<T>(this HashEntry[] hashEntries) where T : new()
         {
             var objectType = typeof(T);
-            if (TypeProperties.ContainsKey(objectType))
+            if (RegisteredTypes.ContainsKey(objectType))
             {
                 var objectAccessor = TypeAccessor.Create(objectType);
                 T objectRead = new T();
 
                 foreach (var hashEntry in hashEntries)
                 {
-                    var propertyType = TypeProperties[typeof(T)][hashEntry.Name];
-                    objectAccessor[objectRead, hashEntry.Name] = FromRedisValueConverters[propertyType](hashEntry.Value);
+                    var propertyType = RegisteredTypes[typeof(T)][hashEntry.Name];
+                    if (ImplementsICollectionOfStrings(propertyType))
+                    {
+                        objectAccessor[objectRead, hashEntry.Name] = RedisValueToCollection(hashEntry.Value);
+                    }
+                    else
+                    {
+                        objectAccessor[objectRead, hashEntry.Name] = RedisConverters[propertyType](hashEntry.Value);
+                    }
                 }
 
                 return objectRead;
